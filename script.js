8bb5f9e91cd77f24e06d79f1cccdc21b5a2afe56
walterdyna <alexdyna@gmail.com>
2024-05-06 15:34:54 -0300

    Refazendo conteudo

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const input = document.querySelector("#main-input")
+
+function cliqueiNoBotão(){
+    alert("Botão clicado com sucesso!")
+}
+
+function cliqueiNoBotão(){
+    alert(input.value)
+}
+
+function digiteiNoImput(){
+    console.log("Digitei no input")
+}
+
+function digiteiNoImput(){
+    console.log(input.value)
+}
